52bfd8544341bacd23ab551cebd35474e8a743e7
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-15 14:46:40 +0100

    add jackson dependency and run test

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -3,6 +3,8 @@ package pl.coderslab.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import pl.coderslab.model.Book;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -11,4 +13,10 @@ public class BookController {
 	public String hello() {
 		return "{hello: World}";
 	}
+
+	@RequestMapping("/helloBook")
+	public Book helloBook() {
+		return new Book(1L, "9788324631766", "Thiniking in Java", "Bruce Eckel", "Helion", "programming");
+	}
+
 }
